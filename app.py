39a67666cd75276901395efbce3523fdf9fbfe6b
Adhithya <rvadhith@gmail.com>
2020-10-23 18:28:15 +0530

    third commit

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ logger = logging.getLogger('HELLO WORLD')
 
 
 UPLOAD_FOLDER = '/home/adhithya/AIkenist/Covid/covidbackend/static//'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
